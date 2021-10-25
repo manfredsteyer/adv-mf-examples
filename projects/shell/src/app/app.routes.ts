42bef3a4b0aee68511012e3d61e867892abef2c3
Manfred Steyer <manfred.steyer@gmx.net>
2021-10-25 23:38:10 +0200

    starter

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
+import { startsWith, WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -6,24 +7,39 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const URL = 'http://localhost:3000/remoteEntry.js';
 
 export const APP_ROUTES: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
 
-    // Your route here:
-    {
-      path: 'flights',
-      loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
-    },
-    {
-      path: '**',
-      component: NotFoundComponent
-    }
+  // Your route here:
+  {
+    path: 'flights',
+    loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
+  },
 
-    // DO NOT insert routes after this one.
-    // { path:'**', ...} needs to be the LAST one.
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+
+  // DO NOT insert routes after this one.
+  // { path:'**', ...} needs to be the LAST one.
 
 ];
 
+
+
+
+  // {
+  //   // path: 'react',
+  //   matcher: startsWith('react'),
+  //   component: WebComponentWrapper,
+  //   data: {
+  //     remoteEntry: 'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+  //     remoteName: 'react',
+  //     exposedModule: './web-components',
+  //     elementName: 'react-element'
+  //   } as WebComponentWrapperOptions
+  // },
